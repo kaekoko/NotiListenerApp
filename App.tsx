873f131f4ecbd323482acc21220ca0bba5e7bcc4
kaekoko <kekoko.mm@gmail.com>
2024-01-17 01:28:45 +0630

    first

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -15,6 +15,8 @@ import {
   Text,
   useColorScheme,
   View,
+  Button,
+  AppState
 } from 'react-native';
 
 import {
@@ -24,95 +26,67 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import RNAndroidNotificationListener from 'react-native-android-notification-listener';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
+  const [hasPermission, setHasPermission] = useState(false)
+
+
+  const handleOnPressPermissionButton = async () => {
+      /**
+       * Open the notification settings so the user
+       * so the user can enable it
+       */
+      RNAndroidNotificationListener.requestPermission()
+  }
+
+  const handleAppStateChange = async (
+      nextAppState: string,
+      force = false
+  ) => {
+      if (nextAppState === 'active' || force) {
+          const status =
+              await RNAndroidNotificationListener.getPermissionStatus()
+          setHasPermission(status !== 'denied')
+      }
+  }
+
+  useEffect(() => {
+ 
+
+    const listener = AppState.addEventListener(
+        'change',
+        handleAppStateChange
+    )
+
+    handleAppStateChange('', true)
+
+    return () => {
+      
+        listener.remove()
+    }
+}, [])
+
+ 
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+      <SafeAreaView >
+          <View>
+              <Text>
+                  {hasPermission
+                      ? 'Allowed to handle notifications'
+                      : 'NOT allowed to handle notifications'}
+              </Text>
+              <Button
+                  title='Open Configuration'
+                  onPress={handleOnPressPermissionButton}
+                  disabled={hasPermission}
+              />
+          </View>
+         
+      </SafeAreaView>
+  )
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- index.js
+++ index.js
@@ -3,7 +3,45 @@
  */
 
 import {AppRegistry} from 'react-native';
+import { RNAndroidNotificationListenerPermissionStatus,RNAndroidNotificationListenerHeadlessJsName } from 'react-native-android-notification-listener';
 import App from './App';
 import {name as appName} from './app.json';
+
+const headlessNotificationListener = async ({ notification }) => {
+    /**
+     * This notification is a JSON string in the follow format:
+     *  {
+     *      "app": string,
+     *      "title": string,
+     *      "titleBig": string,
+     *      "text": string,
+     *      "subText": string,
+     *      "summaryText": string,
+     *      "bigText": string,
+     *      "audioContentsURI": string,
+     *      "imageBackgroundURI": string,
+     *      "extraInfoText": string,
+     *      "groupedMessages": Array<Object> [
+     *          {
+     *              "title": string,
+     *              "text": string
+     *          }
+     *      ]
+     *  }
+     */
+
+    if (notification) {
+        /**
+         * Here you could store the notifications in a external API.
+         * I'm using AsyncStorage here as an example.
+         */
+        console.log("hello")
+    }
+}
+
+AppRegistry.registerHeadlessTask(
+    RNAndroidNotificationListenerHeadlessJsName,
+    () => headlessNotificationListener
+)
 
 AppRegistry.registerComponent(appName, () => App);
