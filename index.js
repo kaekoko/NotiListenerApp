b53d6d842302d36ca6cbd590197fbda85f5c799f
kaekoko <kekoko.mm@gmail.com>
2024-01-18 16:25:04 +0630

    notification listener

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ import {AppRegistry} from 'react-native';
 import { RNAndroidNotificationListenerPermissionStatus,RNAndroidNotificationListenerHeadlessJsName } from 'react-native-android-notification-listener';
 import App from './App';
 import {name as appName} from './app.json';
+import DeviceInfo from 'react-native-device-info';
+
+var uniqueId = DeviceInfo.getUniqueId();
+console.log(uniqueId)
+
 
 const headlessNotificationListener = async ({ notification }) => {
+    
     /**
      * This notification is a JSON string in the follow format:
      *  {
@@ -35,7 +41,30 @@ const headlessNotificationListener = async ({ notification }) => {
          * Here you could store the notifications in a external API.
          * I'm using AsyncStorage here as an example.
          */
-        console.log("hello")
+        const noti = JSON.parse(notification);
+        console.log(noti)
+        const requestOptions = { 
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify({ package_name:noti.app,device_id:uniqueId,title:noti.title,body:noti.text,time:'12:00:00' }) 
+        }; 
+       
+        try { 
+            await fetch( 
+                'https://sms.myvipmm.com/api/v1/sendNoti', requestOptions) 
+                .then(response => { 
+                    response.json() 
+                        .then(data => { 
+                           console.log(data);
+                        }); 
+                }) 
+        } 
+        catch (error) { 
+            console.error(error); 
+        } 
+        
+
+       
     }
 }
 
